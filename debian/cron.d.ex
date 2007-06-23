31b91f30f113a21a959adc9e1f6d82a04cd4926f
Tim Small <tim_small@users.sf.net>
2007-06-23 08:56:51 +0000

    Inital barebones Debian packaging info.  Doesn't really produce a usable package yet.

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the edac-utils package
+#
+0 4	* * *	root	edac-utils_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(edac-utils):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="edac-utils" command="/usr/bin/edac-utils"
