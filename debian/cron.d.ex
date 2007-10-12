a270d8d488d28a9dc9069843ac36baf204aa7fc9
Mark A. Grondona <mgrondona@llnl.gov>
2007-10-12 16:16:50 +0000

    Remove debian directory as requested by debian pkg maintainer

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the edac-utils package
-#
-0 4	* * *	root	edac-utils_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(edac-utils):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="edac-utils" command="/usr/bin/edac-utils"
